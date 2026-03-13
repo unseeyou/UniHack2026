edb677353d78613e99401d3a43bb53f7e97a54b3
unseeyou <unseeyoubestmod@gmail.com>
2026-03-14 09:26:22 +1100

    fix: incorrect return type on error + print instead of using the logger

--- site/utils/trip_time.py
+++ site/utils/trip_time.py
@@ -1,12 +1,13 @@
 import requests
 from datetime import datetime, timedelta
+from constants import app
+
 
 def calculate_day_night_hours(
     start: str, end: str, lat: float, lon: float
 ) -> tuple[float, float]:
-    
-    #converts iso 8601 string to datetime
-    start = datetime.fromisoformat(start) 
+    # converts iso 8601 string to datetime
+    start = datetime.fromisoformat(start)
     end = datetime.fromisoformat(end)
 
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=sunrise,sunset&timezone=Australia/Sydney"
@@ -30,19 +31,22 @@ def calculate_day_night_hours(
                 day_minutes += 1
             else:
                 night_minutes += 1
-            
-            current += timedelta(minutes=1) #same as i++ but with datetime
+
+            current += timedelta(minutes=1)  # same as i++ but with datetime
 
         day_hours = day_minutes / 60
-        night_hours = night_minutes / 60    
-        
+        night_hours = night_minutes / 60
+
         return round(day_hours, 1), round(night_hours, 1)
 
-
     except Exception as e:
-        print(e)
-        return "unknown"
+        app.logger.exception(e)
+        return -1.0, -1.0
 
-    
+
 if __name__ == "__main__":
-    print(calculate_day_night_hours("2026-03-13T06:00:00", "2026-03-13T22:00:00", -33.8688, 151.2093))
+    print(
+        calculate_day_night_hours(
+            "2026-03-13T06:00:00", "2026-03-13T22:00:00", -33.8688, 151.2093
+        )
+    )
