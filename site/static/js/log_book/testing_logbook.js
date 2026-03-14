ed7c6e22cebd1ba18a43258aa6b44d50ba0dfb19
unseeyou <unseeyoubestmod@gmail.com>
2026-03-14 21:21:33 +1100

    feat: overview table with placeholders
    Signed-off-by: unseeyou <unseeyoubestmod@gmail.com>

--- site/static/js/log_book/testing_logbook.js
+++ site/static/js/log_book/testing_logbook.js
@@ -18,16 +18,42 @@ function get_all_trips() {
         }
 
         trips.forEach(trip => {
-            const li = document.createElement("li");
+            const li = document.createElement("tr");
 
             // Format the date nicely
-            const startDate = new Date(trip.start).toLocaleString();
+            const startDate = new Date(trip.start);
+            const formattedDate = new Intl.DateTimeFormat('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                timeZone: 'Australia/Sydney'
+            }).format(startDate);
+            const startTime = startDate.toLocaleTimeString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: 'Australia/Sydney'
+            });
+            const endDate = new Date(trip.end);
+            const endTime = endDate.toLocaleTimeString('en-GB', {
+                hour: '2-digit',
+                minute: '2-digit',
+                timeZone: 'Australia/Sydney'
+            });
             const pointCount = trip.points ? trip.points.length : 0;
 
             li.innerHTML = `
-                <strong>Date:</strong> ${startDate} <br>
-                <span>${pointCount} GPS points collected</span>
-                <hr>
+                <th>${formattedDate}</th>
+                <td>[odometer start]</td>
+                <td>[odometer end]</td>
+                <td>${pointCount}</td>
+                <td>[locations]</td>
+                <td>[road types]</td>
+                <td>[weather]</td>
+                <td>[traffic]</td>
+                <td>${startTime}</td>
+                <td>${endTime}</td>
+                <td>[time day]</td>
+                <td>[time night]</td>
             `;
 
             listElement.appendChild(li);
