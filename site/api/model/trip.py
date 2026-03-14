7d658e79514804a5272f2094b6998f6555709222
Andrew Chen (Siqi) <siqi.chen111@gmail.com>
2026-03-14 12:21:25 +1100

    fix: formatting
    Signed-off-by: unseeyou <unseeyoubestmod@gmail.com>

--- site/api/model/trip.py
+++ site/api/model/trip.py
@@ -3,11 +3,13 @@ from datetime import datetime
 from database.trip.trip import Trip
 from database.trip.point import Point as DBPoint
 
+
 class Point(BaseModel):
     time: datetime
 
     lat: float
     lng: float
+
 
 class AddTrip(BaseModel):
     start: datetime
@@ -19,5 +21,12 @@ class AddTrip(BaseModel):
         return Trip(
             start=self.start,
             end=self.end,
-            points=list(map(lambda point: DBPoint(time=point.time, lat=point.lat, lng=point.lng), self.points))
+            points=list(
+                map(
+                    lambda point: DBPoint(
+                        time=point.time, lat=point.lat, lng=point.lng
+                    ),
+                    self.points,
+                )
+            ),
         )
